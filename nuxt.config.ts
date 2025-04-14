c765fe57aa85169e4cf9de2f7ef555e392c8ee88
Lew <lew.kni@gmail.com>
2025-04-14 08:36:47 +0100

    [TICKET #] - Added in github actions for deployment into GH Pages.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	ssr: true,
-	target: 'static', // not required in Nuxt 3 but good to be explicit
+	nitro: {
+		preset: 'static',
+	},
 	app: {
 		baseURL: '/basic-movie-series-omdb-search/', // 👈 VERY important for GitHub Pages
 	}
