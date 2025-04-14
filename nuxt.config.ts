dba36641b65e7ff99c7cc0eedc4db089e973f018
Lew <lew.kni@gmail.com>
2025-04-14 08:16:30 +0100

    [TICKET #] - Added in gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
+	ssr: true,
+	target: 'static', // not required in Nuxt 3 but good to be explicit
+	app: {
+		baseURL: '/basic-movie-series-omdb-search/', // 👈 VERY important for GitHub Pages
+	}
 	modules: ['@nuxtjs/tailwindcss'],
 	devtools: { enabled: true },
 	components: [
